99a9a6c7d4259f72d49e45659ba0b35646c60bab
Rahul Kumar <rahulkumarpec27@gmail.com>
2026-07-10 17:16:18 +0530

    changes done in age calculator

--- week01/Day01/agecalculator.py
+++ week01/Day01/agecalculator.py
@@ -3,16 +3,14 @@ import re
 dob=input("enter your dob in DD-MM-YYYY format")
 today=str(date.today())
 dobm=(today[5:7])
-print(dobm)
 doby=(today[0:4])
 months=re.findall(r"-\d{2}-",dob)
-print(months)
 months=months[0][1:3]
-print(months)
 years=re.findall(r"\d{4}",dob)
-if int(months)<int(dobm):
-    print(years[0],doby)
-    print(f"You are {int(doby)-int(years[0])} old")
+if int(months)>int(dobm):
+    month=(12-int(months)+int(dobm))
+    print(f"You are {month} months {int(doby)-int(years[0])-1} years old")
 else:
-    print(f"You are {int(doby)-int(years[0])-1} old")
+    month=int(dobm)-int(months)
+    print(f"You are {month} months {int(doby)-int(years[0])} years old")
 
