4f750e55b7397d12fa724ce1c29ac64c2a2013d4
Rahul Kumar <rahulkumarpec27@gmail.com>
2026-07-13 14:34:30 +0530

    added

--- week01/Day02/max.py
+++ week01/Day02/max.py
@@ -1,11 +1,17 @@
 count=int(input("How many numbers you want to add"))
-largest=second_largest=0
-while(count>0):
+a=int(input("enter the number"))
+largest=second_largest=a
+while(count>1):
     num=int(input("enter the number"))
     if num>largest:
         second_largest=largest
         largest=num
     elif num<largest and num>second_largest:
         second_largest=num
+    elif num<largest and second_largest==largest:
+        second_largest=num
     count-=1
-print(f"Largest number is {largest} and second largest is {second_largest}")
+if largest==second_largest: #duplicated numbers only
+    print(f"Largest number is {a} and No second largest number exist")
+else:
+    print(f"Largest number is {largest} and second largest is {second_largest}")
