2f9e6dcc3f0df78c73422d4a710c754901aab3c3
Rahul Kumar <rahulkumarpec27@gmail.com>
2026-07-13 10:01:23 +0530

    ew code of largest amongst three numbers is added

---
+++ week01/Day02/Largest.py
@@ -0,0 +1,9 @@
+a=input("enter first number")
+b=input("enter second number")
+c=input("enter third number")
+if a>b and a>c:
+    print(f"largest is {a}")
+elif b>a and b>c:
+    print(f"largest is {b}")
+elif c>a and c>b:
+    print(f"largest is {c}")
